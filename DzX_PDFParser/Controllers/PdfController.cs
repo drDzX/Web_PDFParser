14681c1036a8fc16fb2a5a5189a4802280a31772
drDzX <dziggy4ever@gmail.com>
2020-10-05 13:27:56 +0200

    PDF Parser

---
+++ DzX_PDFParser/Controllers/PdfController.cs
@@ -0,0 +1,21 @@
+﻿using DzX_PDFParser.PDFParser;
+using iText.Kernel.Geom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DzX_PDFParser.Controllers
+{
+    public class PdfController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return  PDFParser.PDFParser.ManipulatePdf();
+        }
+
+    }
+}
---
+++ DzX_PDFParser/PDFParser/PDF.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using iText.Forms.Xfdf;
+using iText.Kernel.Font;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Data;
+using iText.Kernel.Pdf.Canvas.Parser.Filter;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+
+namespace DzX_PDFParser.PDFParser
+{
+
+    public static class PDFExtract
+    {
+
+        private static FieldInfo locationalResultField = typeof(LocationTextExtractionStrategy).GetField("locationalResult", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public static string[] ExtractText(this PdfPage page, params Rectangle[] rects)
+        {
+            var textEventListener = new LocationTextExtractionStrategy();
+            PdfTextExtractor.GetTextFromPage(page, textEventListener);
+            string[] result = new string[rects.Length];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = textEventListener.GetResultantText(rects[i]);
+            }
+            return result;
+        }
+
+        public static String GetResultantText(this LocationTextExtractionStrategy strategy, Rectangle rect)
+        {
+            IList<TextChunk> locationalResult = (IList<TextChunk>)locationalResultField.GetValue(strategy);
+            List<TextChunk> nonMatching = new List<TextChunk>();
+            foreach (TextChunk chunk in locationalResult)
+            {
+                ITextChunkLocation location = chunk.GetLocation();
+                Vector start = location.GetStartLocation();
+                Vector end = location.GetEndLocation();
+                if (!rect.IntersectsLine(start.Get(Vector.I1), start.Get(Vector.I2), end.Get(Vector.I1), end.Get(Vector.I2)))
+                {
+                    nonMatching.Add(chunk);
+                }
+            }
+            nonMatching.ForEach(c => locationalResult.Remove(c));
+            try
+            {
+                return strategy.GetResultantText();
+            }
+            finally
+            {
+                nonMatching.ForEach(c => locationalResult.Add(c));
+            }
+        }
+
+    }
+    public static class PDFParser
+    {
+        public static readonly String SRC = "E:/LicenseCertificate.pdf";
+        public static String[] ManipulatePdf()
+        {
+            PdfDocument pdfDoc = new PdfDocument(new PdfReader(SRC));
+            Rectangle rect = new Rectangle(135, 685, 150, 25);
+            Rectangle rect2 = new Rectangle(410, 685, 150, 25);
+            Rectangle[] rectArray = new Rectangle[] { rect ,rect2};
+            return PDFExtract.ExtractText(pdfDoc.GetFirstPage(),rectArray);
+        }
+    }
+}
